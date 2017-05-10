8d1896e251b7d3b3e099343f9a6ca42548dbe275
LTFranklin <louis.t.franklin@gmail.com>
2017-05-10 22:49:15 +0100

    Minor edits
    Removed uneeded methods, rearanged to better suit spec, added more
    comments

--- Coursework.pl
+++ Coursework.pl
@@ -1,11 +1,17 @@
-main2(In,X):-
-    m(In,Temp),
-    sentence(Temp,[],X),
-    break(X).
+main2(In):-
+    percieve(In,Percepts),
+    action(Percepts).
 
-break([[X,Y]|Z]):-
-    rule(X,[Y],Z,Out),
+percieve(In,Out):-
+    syn(In,Temp),
+    sentence(Temp,[],Out).
+
+action([[NP,V]|NP2]):-
+    rule(NP,[V],NP2,Out),
     write(Out).
+
+
+
 
 rule([det(a),adj(very),adj(short),noun(command)],[verb(listing)],[det(the),adj(current),noun(directory)],'ls').
 rule([det(the),adj(current),noun(directory)],[verb(viewed)],[det(in),adj(more),adj(fine),noun(detail)],'ls-la').
@@ -14,34 +20,12 @@ rule([det(a),noun(command)],[verb(moves)],[det(to),det(a),adj(parent),noun(direc
 rule([det(the),noun(command)],[verb(prints)],[det(the),adj(current),noun(directory)],'pwd').
 rule([det(the),noun(command)],[verb(types)],[det(the),adj(file),noun('08226.txt')],'cat 08226.txt').
 
-/*rule(NP,V,NP2,'cd'):-
-    member(noun(command),NP),
-    member(verb(moving),V),
-    member(det(to),NP2),
-    member(adj(higher),NP2),
-    member(noun(directory),NP2).
-rule(NP,V,NP2,'cd'):-
-    member(noun(command),NP),
-    member(verb(moves),V),
-    member(det(to),NP2),
-    member(adj(parent),NP2),
-    member(noun(directory),NP2).
-rule(NP,V,NP2,'pwd'):-
-    member(noun(command),NP),
-    member(verb(prints),V),
-    member(adj(current),NP2),
-    member(noun(directory),NP2).
-rule(NP,V,NP2,'cat 08226.txt'):-
-    member(noun(command),NP),
-    member(verb(types),V),
-    member(adj(file),NP2),
-    member(noun('08226.txt'),NP2).*/
-
-
 sentence([],Out,Temp):-
+    %reverses list to be in the correct order
     myRev(Out,[],Temp).
 
 sentence(In,Out,Temp):-
+    %NP->VP or just VP
     nsentence(In,Out,Temp);
     vsentence(In,Out,Temp).
 
@@ -50,7 +34,9 @@ nsentence([],Out,Temp):-
 nsentence(In,Out,Temp):-
     np(In,Out,Temp).
 nsentence(Rest,Out,Temp):-
+    %reverses list to be in the correct order
     myRev(Out,[],Nout),
+    %brackets to categorise NPs
     vsentence(Rest,[Nout],Temp).
 
 vsentence([],Out,Temp):-
@@ -58,7 +44,9 @@ vsentence([],Out,Temp):-
 vsentence(In,Out,Temp):-
     vp(In,Out,Temp).
 vsentence(Rest,Out,Temp):-
+    %reverses list to be in the correct order
     myRev(Out,[],Nout),
+    %brackets to categorise VP
     sentence(Rest,[Nout],Temp).
 
 
@@ -76,6 +64,7 @@ np([Words],Output,Temp):-
 np([Words],Out,Temp):-
     np(Words,Out,Temp),
     pp(Words,Out,Temp).
+%end
 np([Words],Out,Temp):-
     nsentence(Words,Out,Temp).
 
@@ -87,6 +76,7 @@ np2([Word|Rest],Out,Temp):-
 np2([Word|Rest],Out,Temp):-
     isAdj(Word,Output),
     np2(Rest,[Output|Out],Temp).
+%end
 np2([Words],Out,Temp):-
     nsentence(Words,Out,Temp).
 
@@ -115,19 +105,23 @@ vp([Word|Rest],Out,Temp):-
 vp([Word|Rest],Out,Temp):-
     isVerb(Word,Output),
     np(Rest,[Output|Out],Temp).
+%end
 vp([Words],Out,Temp):-
     vsentence(Words,Out,Temp).
 
-
+%end
 verbAd([],Out,Out).
+%checks for a verb
 verbAd([Word|Rest],[Output|Out],Temp):-
     isVerb(Word,Output),
     verbAd(Rest,Out,Temp).
+%checks for an adverb(never used)
 verbAd([Word|Rest],[Output|Out],Temp):-
     isAdverb(Word,Output),
     verbAd(Rest,Out,Temp).
 
 
+%compares the word with the dictionary to find its type
 isNoun(Word,noun(Word)):-
     noun(Word).
 
@@ -148,6 +142,27 @@ isAdverb(Word,adverb(Word)):-
 
 
 
+myRev([],Temp,Temp).
+myRev([H|Tail],Temp,OutList):-
+	myRev(Tail,[H|Temp],OutList).
+
+syn(In,Out):-
+    switch(In,[],Out).
+
+switch([],Temp,Out):-
+    myRev(Temp,[],Out).
+switch([H|List],Temp,Out):-
+    swap(H,X),
+    switch(List,[X|Temp],Out).
+switch([H|List],Temp,Out):-
+    switch(List,[H|Temp],Out).
+
+swap(In,H):-
+    synonym([H|X]),
+    member(In,X).
+
+synonym([directory,folder]).
+synonym([prints,cars,views]).
 noun(command).
 noun(directory).
 noun(detail).
@@ -173,28 +188,3 @@ adj(short).
 adj(current).
 adverb(x).
 
-myRev([],Temp,Temp).
-myRev([H|Tail],Temp,OutList):-
-	myRev(Tail,[H|Temp],OutList).
-
-m(In,Out):-
-    switch(In,[],Out).
-
-switch([],Temp,Out):-
-    myRev(Temp,[],Out).
-switch([H|List],Temp,Out):-
-    swap(H,X),
-    switch(List,[X|Temp],Out).
-switch([H|List],Temp,Out):-
-    switch(List,[H|Temp],Out).
-
-swap(In,H):-
-    synonym([H|X]),
-    member(In,X).
-
-synonym([directory,folder]).
-synonym([prints,cars,views]).
-
-
-foo([[[det(a), adj(short), noun(command)], verb(listing)], det(the), adj(current), noun(directory)]).
-      %[[A,B]|C]
